9e413074155db135c4c5c775b6f793a26cd1c5c3
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-16 23:29:00 +0100

    refactor: update styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
+/* eslint-disable react/style-prop-object */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#131016',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Quarta, 16 de Novembro de 2022</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
