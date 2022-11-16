f8c45a8446a494fcdc86e9a6bf15343b95340c9f
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-17 00:07:26 +0100

    chore: add new screen and styles

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,27 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+  const handleAddParticipant = () => {
+    console.log('click');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
